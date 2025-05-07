31ffb43a9602ac6ee282a80a633cb8e6ce13dfe8
SkarbrandtheExiled <131450052+SkarbrandtheExiled@users.noreply.github.com>
2025-05-07 15:13:13 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10254164_LukeC_GR2_PROG7311_A2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
