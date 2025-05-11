b5d4ded9ae8c92224626f116825164d05f07e671
SkarbrandtheExiled <131450052+SkarbrandtheExiled@users.noreply.github.com>
2025-05-11 23:21:16 +0200

    Attempt at managing the database enitities

--- Models/employeeModel.cs
+++ Models/employeeModel.cs
@@ -4,9 +4,10 @@ namespace ST10254164_LukeC_GR2_PROG7311_A2.Models
 {
     public class employeeModel
     {
-        public int productID { get; set; }
-        public int farmerID { get; set; }
+        public int employeeID { get; set; }
         public required string farmerName { get; set; } 
         public DateTime dateAdded { get; set; } 
+
+        public ICollection<farmerModel> Product { get; set; }
     }
 }
---
+++ Models/productModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ST10254164_LukeC_GR2_PROG7311_A2.Models
+{
+    public class productModel
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public string Category { get; set; }
+        public DateTime productCreationDate { get; set; }
+        public DateTime dateAdded { get; set; }
+
+        public string UserId { get; set; } 
+        public ApplicationUser User { get; set; }
+    }
+}
